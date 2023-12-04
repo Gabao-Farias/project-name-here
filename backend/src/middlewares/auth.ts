3768da5d402515e3bd75b52c103ec23e2519da36
Gabriel Taborda Farias <gabrielfariasbass@gmail.com>
2023-12-04 17:20:59 -0300

    middleware for auth

---
+++ backend/src/middlewares/auth.ts
@@ -0,0 +1,28 @@
+import "dotenv/config";
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+export const authenticateToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.headers.authorization;
+  const token = (authHeader && authHeader?.split(" ")[1]) || "";
+
+  if (token === undefined) {
+    return res.sendStatus(401);
+  }
+
+  const jwtSecret = process.env.ACCESS_TOKEN_SECRET || "";
+
+  verify(token, jwtSecret, (err, user) => {
+    if (err) {
+      return res.sendStatus(403);
+    }
+
+    req.user = user;
+
+    next();
+  });
+};
