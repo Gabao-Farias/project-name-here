a05a50e5b1e4c3ab73a132fb8ed365c5530d408e
Gabriel Taborda Farias <gabrielfariasbass@gmail.com>
2023-12-04 14:56:57 -0300

    adding initial logic for signup

--- backend/app.ts
+++ backend/app.ts
@@ -1,9 +1,12 @@
+import { hash } from "bcrypt";
 import "dotenv/config";
 import express, { Request, Response } from "express";
 import { getMachineHealth } from "./machineHealth";
 
 const app = express();
 const port = 3001;
+
+const users: any[] = [];
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
@@ -15,6 +18,30 @@ app.post("/machine-health", (req: Request, res: Response) => {
     res.status(400).json(result);
   } else {
     res.json(result);
+  }
+});
+
+app.post("/users", (req: Request, res: Response) => {
+  res.status(200).json(users);
+});
+
+// Endpoint to get machine health score
+app.post("/auth/signup", async (req: Request, res: Response) => {
+  try {
+    const hashedPassword = await hash(req?.body.password, 10);
+
+    const user = {
+      name: req.body.name,
+      password: hashedPassword,
+    };
+
+    users.push(user);
+
+    console.log(users);
+
+    res.status(201).send();
+  } catch (error) {
+    res.status(500).send();
   }
 });
 
