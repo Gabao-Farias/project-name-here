4f611bde7acad2a9237fe2a5ee4e82938a573c2c
Gabriel Taborda Farias <gabrielfariasbass@gmail.com>
2023-12-05 10:18:32 -0300

    initializing database connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,16 @@
 import "dotenv/config";
+import "reflect-metadata";
 import { app } from "./app";
+import { bootstrap } from "./bootstrap";
 
 const port = 3001;
 
-app.listen(port, () => {
-  console.log(`API is listening at http://localhost:${port}`);
-});
+const main = async () => {
+  await bootstrap();
+
+  app.listen(port, () => {
+    console.log(`API is listening at http://localhost:${port}`);
+  });
+};
+
+main();
