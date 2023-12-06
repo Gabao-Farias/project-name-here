24df38c5ba76593ec32050e0240281f1dddeedce
Gabriel Taborda Farias <gabrielfariasbass@gmail.com>
2023-12-06 10:03:46 -0300

    types

--- native-app/types/api.auth.d.ts
+++ native-app/types/api.auth.d.ts
@@ -7,3 +7,11 @@ declare type AuthSignInResponseBody = {
   accessToken: string;
   refreshToken: string;
 };
+
+declare type AuthRefreshRequestBody = {
+  refreshToken: string;
+};
+
+declare type AuthRefreshResponseBody = {
+  accessToken: string;
+};
