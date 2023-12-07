07ee2a61b42a4a80f27dce5b65d1335fe60b5232
Gabriel Taborda Farias <gabrielfariasbass@gmail.com>
2023-12-07 13:59:39 -0300

    repositories

--- backend/src/database/repositories/index.ts
+++ backend/src/database/repositories/index.ts
@@ -1,7 +1,9 @@
 import { datasource } from "../datasource";
 import { RefreshToken, User } from "../entities";
 import { AssemblyLines } from "../entities/assemblyLines.entity";
+import { MachineHealth } from "../entities/machineHealth.entity";
 import { MachineHistory } from "../entities/machineHistory.entity";
+import { MachineScore } from "../entities/machineScore.entity";
 import { PaintingStation } from "../entities/paitingStation.entity";
 import { QualityControlStation } from "../entities/qualityControlStation.entity";
 import { Welding } from "../entities/welding.entity";
@@ -17,3 +19,6 @@ export const assemblyLinesRepository = datasource.getRepository(AssemblyLines);
 export const qualityControlStationRepository = datasource.getRepository(
   QualityControlStation
 );
+
+export const machineHealthRepository = datasource.getRepository(MachineHealth);
+export const machineScoreRepository = datasource.getRepository(MachineScore);
