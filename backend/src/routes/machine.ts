ce2194b2cefe64e8dd7cf189589c144b8c20c9d0
Gabriel Taborda Farias <gabrielfariasbass@gmail.com>
2023-12-07 14:51:49 -0300

    using save handler on request health calculation

--- backend/src/routes/machine.ts
+++ backend/src/routes/machine.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from "express";
 import { getMachineHealth } from "../functions";
+import { storeMachineHistory } from "../handlers";
 import { authenticateToken } from "../middlewares";
 
 const machineRouter = Router();
@@ -8,10 +9,15 @@ machineRouter.use(authenticateToken);
 
 // Endpoint to get machine health score
 machineRouter.post("/health", (req: Request, res: Response) => {
+  const userId = req.user?.user_id || "";
+
   const result = getMachineHealth(req);
+
   if (result.error) {
     res.status(400).json(result);
   } else {
+    storeMachineHistory(userId, req.body, result);
+
     res.json(result);
   }
 });
