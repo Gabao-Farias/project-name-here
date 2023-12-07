ae3964dd3afffeef1a27c0e62ce8c315a5df651d
Gabriel Taborda Farias <gabrielfariasbass@gmail.com>
2023-12-07 09:19:41 -0300

    adding repositories

--- backend/src/database/repositories/index.ts
+++ backend/src/database/repositories/index.ts
@@ -1,5 +1,19 @@
 import { datasource } from "../datasource";
 import { RefreshToken, User } from "../entities";
+import { AssemblyLines } from "../entities/assemblyLines.entity";
+import { MachineHistory } from "../entities/machineHistory.entity";
+import { PaintingStation } from "../entities/paitingStation.entity";
+import { QualityControlStation } from "../entities/qualityControlStation.entity";
+import { Welding } from "../entities/welding.entity";
 
 export const userRepository = datasource.getRepository(User);
 export const refreshTokenRepository = datasource.getRepository(RefreshToken);
+export const machineHistoryRepository =
+  datasource.getRepository(MachineHistory);
+export const weldingRepository = datasource.getRepository(Welding);
+export const paitingStationRepository =
+  datasource.getRepository(PaintingStation);
+export const assemblyLinesRepository = datasource.getRepository(AssemblyLines);
+export const qualityControlStationRepository = datasource.getRepository(
+  QualityControlStation
+);
